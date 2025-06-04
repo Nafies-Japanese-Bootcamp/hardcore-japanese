52ed6120118ab7b9774dd822cde7a5c7a6d51456
nafies1 <ghuroba.muslim@gmail.com>
2025-06-05 05:02:06 +0700

    Fix github pages baseUrl, etc

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   url: 'https://hardcore-japanese.pages.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/hardcore-japanese/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
